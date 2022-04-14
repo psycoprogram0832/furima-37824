edfe2e6fecd66c3fe50d164bc1044fabb405979a
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-04-14 16:29:54 +0900

    ユーザー管理機能-修正＃２

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
   validates :birth_day, presence: true
 end
