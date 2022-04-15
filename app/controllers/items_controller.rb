38c2ccfd89853940afec732ac380de8fef1f901b
psycoprogram0832 <raidoubiz0832@gmail.com>
2022-04-15 10:37:32 +0900

    出品情報入力画面の完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if 
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:message).permit(:item, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :image, :introduction, :price, :condition_id, :shipment_id,:category_id, :prefecture_id, :delivery_charge_id,).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,12 @@ class Item < ApplicationRecord
   validates :product_name, presence: true
   validates :introduction, presence: true
   validates :image, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipment_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 0, message: "can't be blank" }
+  with_options presence: true, format: { with:/\A[0-9]+\z/ } do
+    validates :price,numericality: {only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9_999_999},presence: { message:"cant be blank" }
+  end
 end
